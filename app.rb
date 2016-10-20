262438e03c8f60f6e0cd813e2989710769c0d48e
Anna Leach <anna.leach@zopa.com>
2016-10-20 18:45:38 +0100

    Amend routes

--- app.rb
+++ app.rb
@@ -2,35 +2,41 @@ require 'sinatra'
 
 class MySite < Sinatra::Base
 
+  #new sprockets environment
+  set :environment, Sprockets::Environment.new
+
+  environment.append_path "public/css"
+  environment.append_path "public/images"
+
   get '/' do
     erb :'index'
   end
 
   get '/blog' do
-    erb :'/blog/blog'
+    erb :'/blog', :views => File.dirname(__FILE__) + '/blog'
   end
 
   get '/blog/?*' do
-    p "!!!!"
-    p request.path
     jekyll_blog(request.path)
   end
 
   def jekyll_blog(path)
     file_path = File.join(File.dirname(__FILE__), '/blog/_site', path.gsub('/blog', ''))
     p "----------------------------------------"
-    p file_path
     file_path = File.join(file_path, 'blog.erb') unless file_path =~ /\.[a-z]+$/i
 
     if File.exist?(file_path)
-      file = File.open(file_path, a)
+      file = File.open(file_path, 'r+')
       contents = file.read
       file.close
 
       p contents
 
     end
-
   end
+
+
+
+
 
 end
